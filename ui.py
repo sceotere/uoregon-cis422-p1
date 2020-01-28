3a1cbf35ecd00da06637b327e83230babe1a6c65
Ben <Benverney@yahoo.com>
2020-01-27 22:17:36 -0800

    Prototype for UI. Highlighting names / arrowkey input

--- ui.py
+++ ui.py
@@ -10,6 +10,9 @@ Date Last Modified: 		Last Modified by:		Completed:
 from tkinter import *
 
 
+listOfNames = ["Bethany", "Mikayla", "Joseph", "Olivia", "Ben"]
+
+
 # Button Commands
 def command1():
 	pass
@@ -29,22 +32,27 @@ def construct():
 	#will come back to this -op
 
 win = Tk()
+#win.iconbitmap()
+#do this if I have time.
 
 #title of the window
 win.title("422 Cold Call")
 
 #labels and buttons
-lbl1 = Label(text='Cold Call', bg = "#002547", fg= "white", font=("Arial", 40), relief = SUNKEN)
-lbl1.pack(expand = 1)
+#lbl1 = Label(text='Cold Call', bg = "#002547", fg= "white", font=("Arial", 40), relief = SUNKEN)
+#lbl1.pack(expand = 1)
 
-but1 =Button(text = "first option here", highlightbackground = "#002547", padx = 10) #command = *put command here* 
-but1.pack(expand = 1)
+slot0 =Label(text = listOfNames[0], highlightbackground = "#002547", padx = 10) #command = *put command here*
+slot0.grid(row=0, column=0)
 
-but2 =Button(text = "second option here", highlightbackground = "#002547", padx = 10, anchor = N) #command = *put command here*
-but2.pack(expand = 1)
+slot1 =Label(text = listOfNames[1], highlightbackground = "#002547", padx = 10, anchor = N) #command = *put command here*
+slot1.grid(row=0, column=1)
 
-but2 =Button(text = "third option here", highlightbackground = "#002547", padx = 10, anchor = N) #command = *put command here*
-but2.pack(expand = 1)
+slot2 =Label(text = listOfNames[2], highlightbackground = "#002547", padx = 10, anchor = N) #command = *put command here*
+slot2.grid(row=0, column=2)
+
+slot3 =Label(text = listOfNames[3], highlightbackground = "#002547", padx = 10, anchor = N) #command = *put command here*
+slot3.grid(row=0, column=3)
 
 #sets window size (which doesn't work atm) and background color
 win.config(width=700, height=500)
@@ -54,5 +62,45 @@ win.config(bg="#002547") #dark slate gray
 # ent2 = Entry()
 # lbl2.grid(column=0, row=1, padx=5, pady=5)
 # ent2.grid(column=1, row=1, padx=5, pady=5)
+
+listOfSlots = [slot0, slot1, slot2, slot3]
+currentNames = [0,1,2,3]
+currentSlot = 0
+slot0.config({"background": "Blue"})
+slot0.config({"foreground": "White"})
+
+def leftPress(event):
+	global currentSlot
+	if currentSlot == 0:
+		return
+	listOfSlots[currentSlot].config({"background": "White"})
+	listOfSlots[currentSlot].config({"foreground": "Black"})
+	currentSlot -= 1
+	listOfSlots[currentSlot].config({"background": "Blue"})
+	listOfSlots[currentSlot].config({"foreground": "White"})
+
+def rightPress(event):
+	global currentSlot
+	if currentSlot == 3:
+		return
+	listOfSlots[currentSlot].config({"background": "White"})
+	listOfSlots[currentSlot].config({"foreground": "Black"})
+	currentSlot += 1
+	listOfSlots[currentSlot].config({"background": "Blue"})
+	listOfSlots[currentSlot].config({"foreground": "White"})
+
+def upPress(event):
+	i = currentSlot
+	while i < 3:
+		listOfSlots[i].config({"text" : listOfSlots[i + 1].cget("text")})
+		i += 1
+	listOfSlots[3].config({"text" : "test"})
+
+
+win.bind('<Left>', leftPress)
+win.bind('<Right>', rightPress)
+win.bind('<Up>', upPress)
+
+
 
 win.mainloop()
