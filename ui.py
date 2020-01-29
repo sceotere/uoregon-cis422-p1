45e262242a203c75e5d2d47f2dbd73797bdd4acc
Olivia Pannell <livvieolive@gmail.com>
2020-01-29 14:57:07 -0800

    import opens directory

--- ui.py
+++ ui.py
@@ -8,22 +8,22 @@ Date Last Modified: 		Last Modified by:		Completed:
 """
 
 from tkinter import *
-import tkinter.font
-
+from tkinter import filedialog
 
 listOfNames = ["Bethany", "Mikayla", "Joseph", "Olivia", "Ben"]
 
 
 # Button Commands
-def command1():
+def hlp():
 	pass
 	#do something here
 
-def command2():
+def imprt():
+	filepath = filedialog.askopenfilename(initialdir="./..")
 	pass
 	#do something here
 
-def command3():
+def exprt():
 	pass
 	#do another thing
 
@@ -44,28 +44,28 @@ win.config(bg="#002547") #dark slate gray
 win.title("422 Cold Call")
 
 #labels and buttons
-lbl1 = Label(text='On Deck:', bg = "#002547", fg= "white", font=("Arial", 16))
+lbl1 = Label(win, text='On Deck:', bg = "#002547", fg= "white", font=("Arial", 16))
 lbl1.grid(row = 0, column = 1, padx=5, pady=5)
 
-slot0 =Label(text = listOfNames[0], highlightbackground = "#002547", padx = 10) #command = *put command here*
+slot0 =Label(win, text = listOfNames[0], highlightbackground = "#002547", padx = 10)
 slot0.grid(row = 1, column = 1, padx=5, pady=5)
 
-slot1 =Label(text = listOfNames[1], highlightbackground = "#002547", padx = 10, anchor = N) #command = *put command here*
+slot1 =Label(win, text = listOfNames[1], highlightbackground = "#002547", padx = 10, anchor = N) 
 slot1.grid(row = 1, column = 2, padx=5, pady=5)
 
-slot2 =Label(text = listOfNames[2], highlightbackground = "#002547", padx = 10, anchor = N) #command = *put command here*
+slot2 =Label(win, text = listOfNames[2], highlightbackground = "#002547", padx = 10, anchor = N) 
 slot2.grid(row = 1, column = 3, padx=5, pady=5)
 
-slot3 =Label(text = listOfNames[3], highlightbackground = "#002547", padx = 10, anchor = N) #command = *put command here*
+slot3 =Label(win, text = listOfNames[3], highlightbackground = "#002547", padx = 10, anchor = N) 
 slot3.grid(row = 1, column = 4, padx=5, pady=5)
 
-b1 =Button(win, text = "help", highlightbackground = "#002547", padx = 10) #command = *put command here*
+b1 =Button(win, text = "help", highlightbackground = "#002547", padx = 10, command = hlp)
 b1.place(relx=1.0, rely=1.0, anchor=SE)
 
-b2 = Button(win, text = "import", highlightbackground = "#002547", padx = 10)
+b2 = Button(win, text = "import", highlightbackground = "#002547", padx = 10, command = imprt)
 b2.place(relx=0.0, rely=1.0, anchor=SW)
 
-b3 = Button(win, text = "export", highlightbackground = "#002547", padx = 10)
+b3 = Button(win, text = "export", highlightbackground = "#002547", padx = 10, command = exprt)
 b3.place(relx=0.20, rely=1.0, anchor=SW)
 
 
