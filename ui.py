76e8040ee6475c78ba06aeca633d1dd502c642c1
Joseph Goh <jgoh2@uoregon.edu>
2020-01-31 05:47:48 -0800

    Reformatted file according to PEP standards

--- ui.py
+++ ui.py
@@ -6,7 +6,7 @@ Description: Contains the Tkinter GUI window
 Authors: Olivia Pannell, Ben Verney, Joseph Goh
 
 Date Last Modified: 		Last Modified by:		Completed:
-1/23/20  					Olivia Pannell			Basic window and structure					
+1/31/20  					Joseph Goh			Basic window and structure
 """
 
 from tkinter import *
@@ -14,85 +14,91 @@ from tkinter import filedialog
 
 listOfNames = ["Bethany", "Mikayla", "Joseph", "Olivia", "Ben"]
 
-#Import filepath and send it to I/O
+
+# Import filepath and send it to I/O
 def imprt():
-	filepath = filedialog.askopenfilename(initialdir="./..")
-	#send to I/O
-	pass
+    filepath = filedialog.askopenfilename(initialdir="./..")
+    # send to I/O
+    pass
 
-#Export
+
+# Export
 def exprt():
-	pass
-	#do another thing
+    pass
 
+
+# do another thing
 
 
 def leftPress(event):
-	global currentSlot
-	if currentSlot == 0:
-		return
-	listOfSlots[currentSlot].config({"background": "#69779b"})
-	listOfSlots[currentSlot].config({"foreground": "White"})
-	currentSlot -= 1
-	listOfSlots[currentSlot].config({"background": "White"})
-	listOfSlots[currentSlot].config({"foreground": "Black"})
+    global currentSlot
+    if currentSlot == 0:
+        return
+    listOfSlots[currentSlot].config({"background": "#69779b"})
+    listOfSlots[currentSlot].config({"foreground": "White"})
+    currentSlot -= 1
+    listOfSlots[currentSlot].config({"background": "White"})
+    listOfSlots[currentSlot].config({"foreground": "Black"})
+
 
 def rightPress(event):
-	global currentSlot
-	if currentSlot == 3:
-		return
-	listOfSlots[currentSlot].config({"background": "#69779b"})
-	listOfSlots[currentSlot].config({"foreground": "White"})
-	currentSlot += 1
-	listOfSlots[currentSlot].config({"background": "White"})
-	listOfSlots[currentSlot].config({"foreground": "Black"})
+    global currentSlot
+    if currentSlot == 3:
+        return
+    listOfSlots[currentSlot].config({"background": "#69779b"})
+    listOfSlots[currentSlot].config({"foreground": "White"})
+    currentSlot += 1
+    listOfSlots[currentSlot].config({"background": "White"})
+    listOfSlots[currentSlot].config({"foreground": "Black"})
+
 
 def upPress(event):
-	i = currentSlot
-	while i < 3:
-		listOfSlots[i].config({"text" : listOfSlots[i + 1].cget("text")})
-		i += 1
-	listOfSlots[3].config({"text" : "test"})
+    i = currentSlot
+    while i < 3:
+        listOfSlots[i].config({"text": listOfSlots[i + 1].cget("text")})
+        i += 1
+    listOfSlots[3].config({"text": "test"})
 
-#sets window size and background color
+
+# sets window size and background color
 win = Tk()
 win.geometry("350x150")
-win.config(bg="#002547")	#002547
+win.config(bg="#002547")
 
-#win.iconbitmap()
-#do this if I have time.
+# win.iconbitmap()
+# do this if I have time.
 
-#Title of the window
+# Title of the window
 win.title("422 Cold Call")
 
-#Labels and Buttons
-lbl1 = Label(win, text='On Deck:', bg = "#002547", fg= "white", font=("Arial", 16))
-lbl1.grid(row = 0, column = 1, padx=5, pady=5)
+# Labels and Buttons
+lbl1 = Label(win, text='On Deck:', bg="#002547", fg="white", font=("Arial", 16))
+lbl1.grid(row=0, column=1, padx=5, pady=5)
 
-slot0 =Label(win, text = listOfNames[0], bg = "#69779b", fg = "white", padx = 10, relief = RAISED) #Not sure if we like RAISED or SUNKEN
-slot0.grid(row = 1, column = 1, padx=5, pady=5)
+slot0 = Label(win, text=listOfNames[0], bg="#69779b", fg="white", padx=10,
+              relief=RAISED)  # Not sure if we like RAISED or SUNKEN
+slot0.grid(row=1, column=1, padx=5, pady=5)
 
-slot1 =Label(win, text = listOfNames[1], bg = "#69779b", fg = "white", padx = 10, relief = RAISED)
-slot1.grid(row = 1, column = 2, padx=5, pady=5)
+slot1 = Label(win, text=listOfNames[1], bg="#69779b", fg="white", padx=10, relief=RAISED)
+slot1.grid(row=1, column=2, padx=5, pady=5)
 
-slot2 =Label(win, text = listOfNames[2], bg = "#69779b", fg = "white", padx = 10, relief = RAISED)
-slot2.grid(row = 1, column = 3, padx=5, pady=5)
+slot2 = Label(win, text=listOfNames[2], bg="#69779b", fg="white", padx=10, relief=RAISED)
+slot2.grid(row=1, column=3, padx=5, pady=5)
 
-slot3 =Label(win, text = listOfNames[3], bg = "#69779b", fg = "white", padx = 10, relief = RAISED)
-slot3.grid(row = 1, column = 4, padx=5, pady=5)
+slot3 = Label(win, text=listOfNames[3], bg="#69779b", fg="white", padx=10, relief=RAISED)
+slot3.grid(row=1, column=4, padx=5, pady=5)
 
-lbl2 =Label(win, text = "Help: \nDequeue - up\nFlag - down", bg = "#002547", fg = "white", font=("Arial", 12))
+lbl2 = Label(win, text="Help: \nDequeue - up\nFlag - down", bg="#002547", fg="white", font=("Arial", 12))
 lbl2.place(relx=1.0, rely=1.0, anchor=SE)
 
-b2 = Button(win, text = "Import", highlightbackground = "#002547", padx = 10, command = imprt)
+b2 = Button(win, text="Import", highlightbackground="#002547", padx=10, command=imprt)
 b2.place(relx=0.0, rely=1.0, anchor=SW)
 
-b3 = Button(win, text = "Export", highlightbackground = "#002547", padx = 10, command = exprt)
+b3 = Button(win, text="Export", highlightbackground="#002547", padx=10, command=exprt)
 b3.place(relx=0.20, rely=1.0, anchor=SW)
 
-
 listOfSlots = [slot0, slot1, slot2, slot3]
-currentNames = [0,1,2,3]
+currentNames = [0, 1, 2, 3]
 currentSlot = 0
 slot0.config({"background": "White"})
 slot0.config({"foreground": "Black"})
