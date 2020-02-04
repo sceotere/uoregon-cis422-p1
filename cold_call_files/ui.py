20ead54d942a8206236655ba379a308dac22e917
Olivia Pannell <livvieolive@gmail.com>
2020-02-03 20:45:50 -0800

    Comments, get rid of flag button, export to .txt

--- cold_call_files/ui.py
+++ cold_call_files/ui.py
@@ -3,12 +3,10 @@ CIS 422 Project 1 User Interface
 
 Description: Contains the Tkinter GUI window
 
-Authors: Olivia Pannell, Ben Verney, Joseph Goh
+Authors: Joseph Goh, Olivia Pannell, Ben Verney
 
 Date Last Modified: 		Last Modified by:		Completed:
-2/03/20  					Joseph Goh			    Most functionality
-
-TODO: Update comments
+2/03/20  					Olivia Pannell			    Final
 
 """
 
@@ -20,11 +18,12 @@ from os import path
 from create_queue import *
 from save_data import *
 
-#update the UI to match the internal representation of the queue
+# Update the UI to match the internal representation of the queue
 def update_ui():
     global listOfNames
     global listOfSlots
     global roster
+    # Updates listOfSlots and listOfNames
     for i in range(4):
         current_student = roster.get_student(i)
         listOfNames[i] = current_student.first + " " + current_student.last[0] + "."
@@ -34,8 +33,10 @@ def update_ui():
 # Import filepath and return it for use
 def imprt():
     global roster
-
+    # Opens new window to navigate through users directory
+    # and captures that path in filepath
     filepath = filedialog.askopenfilename(initialdir="./..")
+    #Makes sure path exists and error checks
     if path.exists(filepath):
         roster = Roster(filepath=filepath)
         if roster.error_num != 0:
@@ -49,20 +50,21 @@ def imprt():
 # Export current state of roster to custom filepath
 def exprt():
     global roster
-
+    # Opens new window that the user will return a directory
     filepath = filedialog.asksaveasfilename(initialdir="./..", filetypes=(("TAB-delimited text files", "*.txt"),
-                                                                          ("all files", "*.*")))
+                                                                            ("all files", "*.*")), defaultextension='.txt')
     save_roster(roster, filepath, export=True)
 
 
 # Reset the flags of all the students in the currently loaded roster
 def reset_flags():
     global roster
+    # Reset flag to true for each student
     for i in range(roster.size):
         roster.students[i].set_flag(reset=True)
     save_roster(roster)
 
-
+# Display Error window based on what error is recieved
 def display_error(error_num: int):
     error_msg = {
         0: "No error detected.",
@@ -74,7 +76,7 @@ def display_error(error_num: int):
     messagebox.showwarning("Warning", error_msg[error_num])
 
 
-
+#Changes done if the user presses the left arrow
 def leftPress(event):
     global currentSlot
     global listOfNames
@@ -87,11 +89,12 @@ def leftPress(event):
     listOfSlots[currentSlot].config({"background": "White"})
     listOfSlots[currentSlot].config({"foreground": "Black"})
 
-
+# Changes done if the user presses the right arrow
 def rightPress(event):
     global currentSlot
     global listOfNames
     global listOfSlots
+
     if currentSlot == 3:
         return
     listOfSlots[currentSlot].config({"background": "#69779b"})
@@ -100,7 +103,7 @@ def rightPress(event):
     listOfSlots[currentSlot].config({"background": "White"})
     listOfSlots[currentSlot].config({"foreground": "Black"})
 
-
+# Changes done if the user presses the up arrow
 def upPress(event):
     global roster
     global currentSlot
@@ -111,7 +114,7 @@ def upPress(event):
     save_roster(roster)
     log_call(dequeued)
 
-
+# Changes done if the user presses the down arrow
 def downPress(event):
     global roster
     global currentSlot
@@ -122,7 +125,7 @@ def downPress(event):
     save_roster(roster)
     log_call(dequeued, flagged=True)
 
-
+#Create daily log
 log_startup()
 
 roster = None
@@ -137,19 +140,23 @@ if path.exists("coldcall.ini"):
             roster = Roster(filepath=conf_filepath, use_conf=True)
             log_import(conf_filepath)
 
-# sets window size and background color
+# Runs the window
 win = Tk()
+
+# Sets window size and background color
 win.geometry("600x185")
 win.config(bg="#002547")
+
+#Makes it only resizable horizontally
 win.resizable(True, False)
+
+#Makes the window have top-always functionality
 win.attributes('-topmost', 'true')
-
-
 
 # Title of the window
 win.title("422 Cold Call")
 
-# Labels and Buttons
+# Labels and Buttons that will appear on the window
 lbl1 = Label(win, text='On Deck:', bg="#002547", fg="white", font=("Arial", 16))
 lbl1.grid(row=0, column=1, padx=5, pady=5, sticky=W)
 
@@ -174,20 +181,16 @@ b0.place(relx=0.0, rely=1.0, anchor=SW)
 b1 = Button(win, text="Export", highlightbackground="#002547", padx=10, command=exprt)
 b1.place(relx=0.12, rely=1.0, anchor=SW)
 
-b2 = Button(win, text="Reset Flags", highlightbackground="#002547", padx=10, command=reset_flags)
-b2.place(relx=0.24, rely=1.0, anchor=SW)
+# b2 = Button(win, text="Reset Flags", highlightbackground="#002547", padx=10, command=reset_flags)
+# b2.place(relx=0.24, rely=1.0, anchor=SW)
 
+# Update slots with students names
 listOfSlots = [slot0, slot1, slot2, slot3]
 currentNames = [0, 1, 2, 3]
 currentSlot = 0
-slot0.config({"background": "White"})
-slot0.config({"foreground": "Black"})
 
-win.bind('<Left>', leftPress)
-win.bind('<Right>', rightPress)
-win.bind('<Up>', upPress)
-win.bind('<Down>', downPress)
-
+#or if no roster is 
+#initalized it will say "Press the Import Button"
 if roster is None:
     for i in range(4):
         listOfSlots[i].config({"text": listOfNames[i]})
@@ -196,5 +199,15 @@ elif roster.error_num != 0:
 else:
     update_ui()
 
+# Changes color of original highlighed slot
+slot0.config({"background": "White"})
+slot0.config({"foreground": "Black"})
 
+# Keeps track of user keystrokes
+win.bind('<Left>', leftPress)
+win.bind('<Right>', rightPress)
+win.bind('<Up>', upPress)
+win.bind('<Down>', downPress)
+
+# Runs window!
 win.mainloop()
